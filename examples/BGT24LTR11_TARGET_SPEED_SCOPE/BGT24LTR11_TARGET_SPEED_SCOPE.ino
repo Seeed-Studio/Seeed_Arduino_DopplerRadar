6c74ac1b377bf730334918ec6f12edfd3a5a7929
Baozhu Zuo <zuobaozhu@gmail.com>
2020-02-02 13:02:07 +0800

    Pretty printed the Arduino code with astyle

--- examples/BGT24LTR11_TARGET_SPEED_SCOPE/BGT24LTR11_TARGET_SPEED_SCOPE.ino
+++ examples/BGT24LTR11_TARGET_SPEED_SCOPE/BGT24LTR11_TARGET_SPEED_SCOPE.ino
@@ -1,55 +1,55 @@
 #include "GBT24LTR11.h"
 
 #ifdef __AVR__
-#include <SoftwareSerial.h>
-SoftwareSerial SSerial(2, 3); // RX, TX
-#define COMSerial SSerial
-#define ShowSerial Serial
+    #include <SoftwareSerial.h>
+    SoftwareSerial SSerial(2, 3); // RX, TX
+    #define COMSerial SSerial
+    #define ShowSerial Serial
 
-GBT24LTR11<SoftwareSerial> GBT;
+    GBT24LTR11<SoftwareSerial> GBT;
 #endif
 
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-#define COMSerial Serial
-#define ShowSerial SerialUSB
+    #define COMSerial Serial
+    #define ShowSerial SerialUSB
 
-GBT24LTR11<Uart> GBT;
+    GBT24LTR11<Uart> GBT;
 #endif
 
 #ifdef ARDUINO_ARCH_STM32F4
-#define COMSerial Serial
-#define ShowSerial SerialUSB
+    #define COMSerial Serial
+    #define ShowSerial SerialUSB
 
-GBT24LTR11<HardwareSerial> GBT;
+    GBT24LTR11<HardwareSerial> GBT;
 #endif
 
 void setup() {
-  // put your setup code here, to run once:
-  ShowSerial.begin(9600);
-  COMSerial.begin(115200);
-  GBT.init(COMSerial);
-  while(!ShowSerial);
-  while(!COMSerial);
-  /*
-     MODE 0 -->detection target mode
-     MODE 1 -->I/Q ADC mode
-  */
-  GBT.setMode(0);
-  //Set the speed detection range
-  while (!GBT.setSpeedScope(512, 0));
+    // put your setup code here, to run once:
+    ShowSerial.begin(9600);
+    COMSerial.begin(115200);
+    GBT.init(COMSerial);
+    while (!ShowSerial);
+    while (!COMSerial);
+    /*
+        MODE 0 -->detection target mode
+        MODE 1 -->I/Q ADC mode
+    */
+    GBT.setMode(0);
+    //Set the speed detection range
+    while (!GBT.setSpeedScope(512, 0));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  uint16_t maxspeed, minspeed;
+    // put your main code here, to run repeatedly:
+    uint16_t maxspeed, minspeed;
 
-  //View speed range
-  if (GBT.getSpeedScope(&maxspeed, &minspeed)) {
-    ShowSerial.print("maximum speed: ");
-    ShowSerial.print(maxspeed);
-    ShowSerial.print("     Minimum Speed: ");
-    ShowSerial.println(minspeed);
-  }
-  delay(1000);
+    //View speed range
+    if (GBT.getSpeedScope(&maxspeed, &minspeed)) {
+        ShowSerial.print("maximum speed: ");
+        ShowSerial.print(maxspeed);
+        ShowSerial.print("     Minimum Speed: ");
+        ShowSerial.println(minspeed);
+    }
+    delay(1000);
 
 }
--- src/GBT24LTR11.h
+++ src/GBT24LTR11.h
@@ -2,7 +2,7 @@
 #define __GBT24LTR11__
 #include "Arduino.h"
 
-//address code 
+//address code
 #define GBT24LTR11_MESSAGE_HEAD 0x55
 #define GBT24LTR11_RECEIVE_ADDRESS 0x2A
 #define GBT24LTR11_SEND_ADDRESS 0xA2
@@ -13,7 +13,7 @@
 #define GBT24LTR11_COMMAND_SET_SPEED_SCOPE 0xC3
 #define GBT24LTR11_COMMAND_GET_SPEED_SCOPE 0xC4
 #define GBT24LTR11_COMMAND_SET_MODE 0xC5
-#define GBT24LTR11_COMMAND_GET_MODE 0xC6 
+#define GBT24LTR11_COMMAND_GET_MODE 0xC6
 #define GBT24LTR11_COMMAND_SET_THRESHOLD 0xC7
 #define GBT24LTR11_COMMAND_GET_THRESHOLD 0xC8
 
@@ -27,21 +27,21 @@
 #define GBT24LTR11_MODE_IQADC 0x01
 
 template <class T>
-class GBT24LTR11
-{
-private:
-    T *_serial;
-    unsigned char commandC1[7]={GBT24LTR11_MESSAGE_HEAD,GBT24LTR11_RECEIVE_ADDRESS,
-                                GBT24LTR11_COMMAND_GET_TARGET,
-                                0x00,0x02,0x01,0x42};
+class GBT24LTR11 {
+  private:
+    T* _serial;
+    unsigned char commandC1[7] = {GBT24LTR11_MESSAGE_HEAD, GBT24LTR11_RECEIVE_ADDRESS,
+                                  GBT24LTR11_COMMAND_GET_TARGET,
+                                  0x00, 0x02, 0x01, 0x42
+                                 };
     /* data */
-public:
-    void init(T &serialPort);
+  public:
+    void init(T& serialPort);
     uint16_t getSpeed();
     uint16_t getTargetState();
-    uint8_t getIQADC(uint16_t I_d[],uint16_t Q_d[],uint16_t *len);
-    uint8_t setSpeedScope(uint16_t maxspeed,uint16_t minspeed);
-    uint8_t getSpeedScope(uint16_t *maxspeed,uint16_t *minspeed);
+    uint8_t getIQADC(uint16_t I_d[], uint16_t Q_d[], uint16_t* len);
+    uint8_t setSpeedScope(uint16_t maxspeed, uint16_t minspeed);
+    uint8_t getSpeedScope(uint16_t* maxspeed, uint16_t* minspeed);
     uint8_t setMode(uint16_t mode);
     uint8_t getMode();
     uint8_t setThreshold(uint16_t whreshold);
