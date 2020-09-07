6b5e29b60510958329aa934524263de9906d2e46
Adriaan Bernstein <adriaan@wlan-145-94-159-124.wlan.tudelft.nl>
2020-09-07 13:43:13 +0200

    Refactoring library to reflect name of the transceiver used (BGT24LTR11)

--- examples/BGT24LTR11_TARGET_SPEED_SCOPE/BGT24LTR11_TARGET_SPEED_SCOPE.ino
+++ examples/BGT24LTR11_TARGET_SPEED_SCOPE/BGT24LTR11_TARGET_SPEED_SCOPE.ino
@@ -1,4 +1,4 @@
-#include "GBT24LTR11.h"
+#include "BGT24LTR11.h"
 
 #ifdef __AVR__
     #include <SoftwareSerial.h>
@@ -6,37 +6,37 @@
     #define COMSerial SSerial
     #define ShowSerial Serial
 
-    GBT24LTR11<SoftwareSerial> GBT;
+    BGT24LTR11<SoftwareSerial> BGT;
 #endif
 
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
     #define COMSerial Serial1
     #define ShowSerial SerialUSB
 
-    GBT24LTR11<Uart> GBT;
+    BGT24LTR11<Uart> BGT;
 #endif
 
 #ifdef ARDUINO_ARCH_STM32F4
     #define COMSerial Serial
     #define ShowSerial SerialUSB
 
-    GBT24LTR11<HardwareSerial> GBT;
+    BGT24LTR11<HardwareSerial> BGT;
 #endif
 
 void setup() {
     // put your setup code here, to run once:
     ShowSerial.begin(9600);
     COMSerial.begin(115200);
-    GBT.init(COMSerial);
+    BGT.init(COMSerial);
     while (!ShowSerial);
     while (!COMSerial);
     /*
         MODE 0 -->detection target mode
         MODE 1 -->I/Q ADC mode
     */
-    GBT.setMode(0);
+    BGT.setMode(0);
     //Set the speed detection range
-    while (!GBT.setSpeedScope(512, 0));
+    while (!BGT.setSpeedScope(512, 0));
 }
 
 void loop() {
@@ -44,7 +44,7 @@ void loop() {
     uint16_t maxspeed, minspeed;
 
     //View speed range
-    if (GBT.getSpeedScope(&maxspeed, &minspeed)) {
+    if (BGT.getSpeedScope(&maxspeed, &minspeed)) {
         ShowSerial.print("maximum speed: ");
         ShowSerial.print(maxspeed);
         ShowSerial.print("     Minimum Speed: ");
--- examples/BGT24LTR11_THRESHOLD/BGT24LTR11_THRESHOLD.ino
+++ examples/BGT24LTR11_THRESHOLD/BGT24LTR11_THRESHOLD.ino
@@ -1,32 +1,32 @@
-#include "GBT24LTR11.h"
+#include "BGT24LTR11.h"
 #ifdef __AVR__
     #include <SoftwareSerial.h>
     SoftwareSerial SSerial(2, 3); // RX, TX
     #define COMSerial SSerial
     #define ShowSerial Serial
 
-    GBT24LTR11<SoftwareSerial> GBT;
+    BGT24LTR11<SoftwareSerial> BGT;
 #endif
 
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
     #define COMSerial Serial1
     #define ShowSerial SerialUSB
 
-    GBT24LTR11<Uart> GBT;
+    BGT24LTR11<Uart> BGT;
 #endif
 
 #ifdef ARDUINO_ARCH_STM32F4
     #define COMSerial Serial
     #define ShowSerial SerialUSB
 
-    GBT24LTR11<HardwareSerial> GBT;
+    BGT24LTR11<HardwareSerial> BGT;
 #endif
 
 void setup() {
     // put your setup code here, to run once:
     ShowSerial.begin(9600);
     COMSerial.begin(115200);
-    GBT.init(COMSerial);
+    BGT.init(COMSerial);
     while (!ShowSerial);
     while (!COMSerial);
     /*
@@ -34,10 +34,10 @@ void setup() {
         MODE 1 -->I/Q ADC mode
     */
     //Set mode
-    GBT.setMode(0);
+    BGT.setMode(0);
 
     //set threshold
-    while (!GBT.setThreshold(1024)) {
+    while (!BGT.setThreshold(1024)) {
         ShowSerial.println("set threshold erro");
     }
     ShowSerial.println("set threshold success");
@@ -47,7 +47,7 @@ void loop() {
     // put your main code here, to run repeatedly:
     uint16_t threshold;
     //get threshold
-    threshold = GBT.getThreshold();
+    threshold = BGT.getThreshold();
     if (threshold != 0) {
         ShowSerial.print("get threshold :");
         ShowSerial.println(threshold);
